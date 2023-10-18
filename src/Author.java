4dfd5817d1f1e8455323460d5eb2321c700d88c4
pai-puella <cat.dyshkant@yandex.ru>
2023-10-18 14:34:47 +0300

    Added equals to class Author

--- src/Author.java
+++ src/Author.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Author {
     private String firstName;
     private String lastName;
@@ -18,5 +20,14 @@ public class Author {
     @Override
     public String toString() {
         return firstName + " " + lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(firstName, author.firstName) &&
+                Objects.equals(lastName, author.lastName);
     }
 }
